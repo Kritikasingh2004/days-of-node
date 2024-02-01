5ea7e9a555db9d01e97857e9f30b68e2ddb39568
Kritika Singh <kritikasingh00.com@gmail.com>
2024-02-01 16:04:10 +0530

    first commit

---
+++ day1/day1.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function readFileContent(filePath) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                console.error(`Error reading file: ${err.code}: ${err.message}`);
+            } else {
+                console.error(`Error reading file: ${err.message}`);
+            }
+        } else {
+            console.log(`File Content:\n${data}`);
+        }
+    });
+}
+
+readFileContent('test-files/file1.txt');
+// Expected Output: Content of file1.txt
+
+readFileContent('test-files/empty-file.txt');
+// Expected Output: (empty string)
+
+readFileContent('test-files/nonexistent-file.txt');
+// Expected Output: Error reading file: ENOENT: no such file or directory...
